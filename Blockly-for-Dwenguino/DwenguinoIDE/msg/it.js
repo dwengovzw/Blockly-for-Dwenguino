133f3c60af88984eff8a257163a804aec85060cd
Tom Neutens <tomneutens@gmail.com>
2020-09-14 15:58:14 +0200

    merged translation fixes from standalone branch into wegostem branch

--- Blockly-for-Dwenguino/DwenguinoIDE/msg/it.js
+++ Blockly-for-Dwenguino/DwenguinoIDE/msg/it.js
@@ -117,6 +117,21 @@ var MSG = {
 };
 
 
+MSG.dropzone = {
+  dictSelectFile: "Select a file.",
+  dictChooseFile: "Choose file",
+  dictDefaultMessage: "Drop files here to upload",
+  dictFallbackMessage: "Your browser does not support drag'n'drop file uploads.",
+  dictFileTooBig: "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.",
+  dictFallbackText: "Please use the fallback form below to upload your files like in the olden days.",
+  dictInvalidFileType: "You can't upload files of this type.",
+  dictResponseError: "Server responded with {{statusCode}} code.",
+  dictCancelUpload: "Cancel upload",
+  dictCancelUploadConfirmation: "Are you sure you want to cancel this upload?",
+  dictRemoveFile: "Remove file",
+  dictMaxFilesExceeded: "You can not upload any more files.",
+}
+
 MSG.simulator = {
   start: "Avvia",
   stop: "Ferma",
