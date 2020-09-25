c468f86923f0b13432f4ad1e96467a109fd76bcc
Tom Neutens <tomneutens@gmail.com>
2020-09-25 15:15:30 +0200

    updated deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,7 +9,7 @@ rm -Rf node_modules
 npm install
 
 # go to backend and stop the previous app version and start the new nodeJS app in bac$
-pm2 stop $(pwd)/backend/index.js
-pm2 start $(pwd)/backend/index.js
+forever stop $(pwd)/backend/index.js
+forever start $(pwd)/backend/index.js
 exit
 
