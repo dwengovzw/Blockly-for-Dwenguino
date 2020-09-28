522b45d25d7956f82fd6dfb2624700e73196901e
Tom Neutens <tomneutens@gmail.com>
2020-09-28 12:02:49 +0200

    changed db credentials

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,9 @@
 # make sure to set production flag of NodeJS
 export NODE_ENV=production
 export PORT=8081
+export MONGODB_URI="mongodb://localhost/dwenguinoblockly"
+export PM2_HOME=/etc/pm2deamon
+
 
 # clean the node_modules directory and reinstall to be sure you have latest version 
 rm -Rf node_modules
@@ -10,6 +13,6 @@ npm install
 
 # go to backend and stop the previous app version and start the new nodeJS app in bac$
 export BUILD_ID=dontKillMePlease 
-pm2 restart $(pwd)/backend/index.js -l ../forever_log.txt&
+pm2 restart $(pwd)/backend/index.js
 exit
 
