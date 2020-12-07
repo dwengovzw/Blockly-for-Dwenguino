7f6696e4982f8ce01a3c55000ed45203219190c8
zimcke <zimcke@gmail.com>
2020-12-07 14:32:28 +0100

    Update of build script to build bundle with production server configuration and clean build directory before deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,12 +6,18 @@ export PORT=8081
 export MONGODB_URI="mongodb://localhost/dwenguinoblockly"
 export PM2_HOME=/etc/pm2deamon
 
+# Remove all files in the deployment directory to make sure no unused files stay behind from the previous deployment
+sudo rm -rf /home/ubuntu/blockly-build/*
 
 # clean the node_modules directory and reinstall to be sure you have latest version 
 rm -Rf node_modules
 npm install
 
+# Overwrite the default server configuration with the server configuration for production
 echo "export default class ServerConfig{static getServerUrl(){return 'http://blockly-backend.dwengo.org';}}" > Blockly-for-Dwenguino/DwenguinoIDE/js/src/server_config.js
+
+# Build the new bundle that uses the new server configuration with webpack 
+npm run build 
 
 # Copy the new files to the deployment directory
 sudo cp -r * /home/ubuntu/blockly-build/
