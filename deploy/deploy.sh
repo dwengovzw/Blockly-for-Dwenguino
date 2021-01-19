fae35550cddda7409275ba85edc0b5e1796acd5d
zimcke <zimcke@gmail.com>
2021-01-19 22:18:27 +0100

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,12 +15,9 @@ npm install
 # Copy the new files to the deployment directory
 sudo cp -r * /home/ubuntu/blockly-build/
 
-sudo rm /home/ubuntu/blockly-build/backend/index.js
-sudo cp /home/ubuntu/certs/index.js /home/ubuntu/blockly-build/backend/
-
 cd /home/ubuntu/blockly-build/
 
 # go to backend and stop the previous app version and restart the new nodeJS app in back
-sudo forever restart backend/index.js || sudo forever start backend/index.js
+forever restart backend/index.js || forever start backend/index.js
 exit
 
