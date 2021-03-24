5a89be85e32bc9c6d000af4c980166513dc63224
zimcke <zimcke@gmail.com>
2021-03-24 19:42:56 +0100

    Update authentication_controller.js

--- backend/controllers/authentication_controller.js
+++ backend/controllers/authentication_controller.js
@@ -105,6 +105,7 @@ exports.register = function(req, res){
 
                 if(process.env.NODE_ENV === 'production'){
                   sgMail.setApiKey(process.env.EMAIL_PASSWORD);
+                  console.log(process.env.EMAIL_PASSWORD);
                   sgMail
                     .send(message)
                     .then(() => {}, error => {
@@ -370,6 +371,7 @@ exports.getPasswordResetCode = function (req, res){
 
         if(process.env.NODE_ENV === 'production'){
           sgMail.setApiKey(process.env.EMAIL_PASSWORD);
+          console.log(process.env.EMAIL_PASSWORD);
           sgMail
             .send(message)
             .then(() => {}, error => {
