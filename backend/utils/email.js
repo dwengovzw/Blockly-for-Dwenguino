0076f03463d30b59639ed7359d4d5b6555a31122
zimcke <zimcke@gmail.com>
2021-04-07 11:47:43 +0200

    Mail server settings

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -4,6 +4,7 @@ import nodemailer from 'nodemailer';
 const emailService = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
