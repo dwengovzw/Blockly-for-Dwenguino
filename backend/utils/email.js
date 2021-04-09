bf34c75682f777791a5d3646c8b038967995054a
zimcke <zimcke@gmail.com>
2021-04-09 10:34:16 +0200

    Update email.js

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 // when working local we use a mock smtp server from Ethereal
-const emailService = nodemailer.createTransport({
+let options = {
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     auth: {
@@ -10,6 +10,8 @@ const emailService = nodemailer.createTransport({
     },
     debug: true,
     logger: true
-});
+}
+console.log(options);
+const emailService = nodemailer.createTransport(options);
 
 export default emailService;
