6367b8d83fb0ec137bdb45a15f559a7cd9f295f7
zimcke <zimcke@gmail.com>
2021-04-09 10:34:55 +0200

    Update authentication_controller.js

--- backend/controllers/authentication_controller.js
+++ backend/controllers/authentication_controller.js
@@ -394,6 +394,7 @@ exports.getPasswordResetCode = function (req, res){
           html: html
         };
 
+        console.log(message);
         emailService.sendMail(message);
 
         res.sendStatus(200);
