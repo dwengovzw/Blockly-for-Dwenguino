1343a50f2f97f876592bb37f34f1cb76758aa411
zimcke <zimcke@gmail.com>
2021-04-14 15:11:05 +0200

    Program saving functionality layout modified

--- Blockly-for-Dwenguino/DwenguinoIDE/js/src/user/user_programs_modal.js
+++ Blockly-for-Dwenguino/DwenguinoIDE/js/src/user/user_programs_modal.js
@@ -19,13 +19,13 @@ class UserProgramsModal {
 
         $('#saveProgramsGrid').append('<form id="saveProgram"></form>');
 
-        $('#saveProgram').append('<div id="saveProgramHeader" class="ui-widget row mb-4"></div>');
+        $('#saveProgram').append('<div id="saveProgramHeader" class="ui-widget row row-header pt-2 pb-2 mt-2 mb-2"></div>');
         $('#saveProgramHeader').append('<div class="col-auto">'+DwenguinoBlocklyLanguageSettings.translateFrom('logging', ['saveCurrentProgram'])+'</div>');
 
         $('#saveProgram').append('<div id="nameInput" class="ui-widget row mb-4"></div>');
         $('#nameInput').append('<label for="nameTag" class="col-md-3">'+ DwenguinoBlocklyLanguageSettings.translateFrom('logging', ['programName'])+'</label>');
         $('#nameInput').append('<input id="nameTag" name="nameTag" class="col-md-4" placeholder="'+ 'myprogram' +'">');
-        $('#nameInput').append('<button id="save_program_button" type="button" class="btn btn-primary col-md-2">'+DwenguinoBlocklyLanguageSettings.translate(['save']) +'</button>');
+        $('#nameInput').append('<button id="save_program_button" type="button" class="btn btn-info col-md-2">'+DwenguinoBlocklyLanguageSettings.translate(['save']) +'</button>');
 
         $("#save_program_button").click(function(){
             self.saveCurrentProgram();
@@ -43,19 +43,19 @@ class UserProgramsModal {
         
         function onDone(userPrograms) {
             
-            $('#programsGrid').append('<div id="programsRow" class="ui-widget row row-header pt-2 mt-2 mb-2"></div>');
-            $('#programsRow').append('<div class="col-md-5"><p>'+ DwenguinoBlocklyLanguageSettings.translateFrom('logging',['programName']) +'</p></div>');
-            $('#programsRow').append('<div class="col-md-3"><p>'+ DwenguinoBlocklyLanguageSettings.translate(['delete']) +'</p></div>');
-            $('#programsRow').append('<div class="col-md-3"><p>'+ DwenguinoBlocklyLanguageSettings.translate(['restore']) +'</p></div>');
+            $('#programsGrid').append('<div id="programsRow" class="ui-widget row row-header pt-2 pb-2 mt-2 mb-2"></div>');
+            $('#programsRow').append('<div class="col-md-5">'+ DwenguinoBlocklyLanguageSettings.translateFrom('logging',['programName']) +'</div>');
+            $('#programsRow').append('<div class="col-md-3">'+ DwenguinoBlocklyLanguageSettings.translate(['delete']) +'</div>');
+            $('#programsRow').append('<div class="col-md-3">'+ DwenguinoBlocklyLanguageSettings.translate(['restore']) +'</div>');
             $('#programsRow').append('<div class="col-md-1"><i class="fa fa-download"></i></div>');
             self._userPrograms = userPrograms;
 
             $.each(userPrograms, function(i, program){
-                $('#programsGrid').append('<div id="programsRow'+i+'" class="ui-widget row row-striped pt-2"></div>');
+                $('#programsGrid').append('<div id="programsRow'+i+'" class="ui-widget row row-striped pt-2 pb-2"></div>');
                 $('#programsRow'+i).append('<div class="col-md-5"><i class="fa fa-edit pr-1"></i><div id="nameProgram'+i+'" contenteditable="true" class="nameProgram">'+ program.program_name +'</div>');
-                $('#programsRow'+i).append('<div class="col-md-3"><button id="deleteProgram'+i+'">'+DwenguinoBlocklyLanguageSettings.translate(['delete'])+'</button></div>');
-                $('#programsRow'+i).append('<div class="col-md-3"><button id="restoreProgram'+i+'">'+DwenguinoBlocklyLanguageSettings.translate(['open'])+'</button></div>');                
-                $('#programsRow'+i).append('<div class="col-md-1"><i id="downloadProgram'+i+'" class="fa fa-download button"></i></div>');
+                $('#programsRow'+i).append('<div class="col-md-3"><button class="btn btn-info" id="deleteProgram'+i+'">'+DwenguinoBlocklyLanguageSettings.translate(['delete'])+'</button></div>');
+                $('#programsRow'+i).append('<div class="col-md-3"><button  class="btn btn-info" id="restoreProgram'+i+'">'+DwenguinoBlocklyLanguageSettings.translate(['open'])+'</button></div>');                
+                $('#programsRow'+i).append('<div class="col-md-1"><i id="downloadProgram'+i+'" class="fa fa-download button text-info"></i></div>');
 
                 $("#restoreProgram"+i).on('click', function(event) {
                     let id = event.target.id;
@@ -143,19 +143,19 @@ class UserProgramsModal {
         
         function onDone(userPrograms) {
             $('#programsGrid').empty();
-            $('#programsGrid').append('<div id="programsRow" class="ui-widget row row-header pt-2 mt-2 mb-2"></div>');
-            $('#programsRow').append('<div class="col-md-5"><p>'+ DwenguinoBlocklyLanguageSettings.translateFrom('logging',['programName']) +'</p></div>');
-            $('#programsRow').append('<div class="col-md-3"><p>'+ DwenguinoBlocklyLanguageSettings.translate(['delete']) +'</p></div>');
-            $('#programsRow').append('<div class="col-md-3"><p>'+ DwenguinoBlocklyLanguageSettings.translate(['restore']) +'</p></div>');
+            $('#programsGrid').append('<div id="programsRow" class="ui-widget row row-header pt-2 pb-2 mt-2 mb-2"></div>');
+            $('#programsRow').append('<div class="col-md-5">'+ DwenguinoBlocklyLanguageSettings.translateFrom('logging',['programName']) +'</div>');
+            $('#programsRow').append('<div class="col-md-3">'+ DwenguinoBlocklyLanguageSettings.translate(['delete']) +'</div>');
+            $('#programsRow').append('<div class="col-md-3">'+ DwenguinoBlocklyLanguageSettings.translate(['restore']) +'</div>');
             $('#programsRow').append('<div class="col-md-1"><i class="fa fa-download"></i></div>');
             self._userPrograms = userPrograms;
 
             $.each(userPrograms, function(i, program){
-                $('#programsGrid').append('<div id="programsRow'+i+'" class="ui-widget row row-striped pt-2"></div>');
+                $('#programsGrid').append('<div id="programsRow'+i+'" class="ui-widget row row-striped pt-2 pb-2"></div>');
                 $('#programsRow'+i).append('<div class="col-md-5"><i class="fa fa-edit pr-1"></i><div id="nameProgram'+i+'" contenteditable="true" class="nameProgram">'+ program.program_name +'</div>');
-                $('#programsRow'+i).append('<div class="col-md-3"><button id="deleteProgram'+i+'">'+DwenguinoBlocklyLanguageSettings.translate(['delete'])+'</button></div>');
-                $('#programsRow'+i).append('<div class="col-md-3"><button id="restoreProgram'+i+'">'+DwenguinoBlocklyLanguageSettings.translate(['open'])+'</button></div>');                
-                $('#programsRow'+i).append('<div class="col-md-1"><i id="downloadProgram'+i+'" class="fa fa-download button"></i></div>');
+                $('#programsRow'+i).append('<div class="col-md-3"><button id="deleteProgram'+i+'" class="btn btn-info">'+DwenguinoBlocklyLanguageSettings.translate(['delete'])+'</button></div>');
+                $('#programsRow'+i).append('<div class="col-md-3"><button id="restoreProgram'+i+'"  class="btn btn-info">'+DwenguinoBlocklyLanguageSettings.translate(['open'])+'</button></div>');                
+                $('#programsRow'+i).append('<div class="col-md-1"><i id="downloadProgram'+i+'" class="fa fa-download button text-info"></i></div>');
 
                 $("#restoreProgram"+i).on('click', function(event) {
                     let id = event.target.id;
