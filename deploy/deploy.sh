7ba2602c4c922c9b8ef4455d5e9a6cab255ba020
Tom Neutens <tomneutens@gmail.com>
2021-06-17 11:11:35 +0200

    added line to deploy script to give jenkins write access to the compilation folder

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -14,6 +14,9 @@ sudo cp -r /var/lib/jenkins/workspace/blockly-build/ /home/ubuntu/
 
 cd /home/ubuntu/blockly-build/backend
 
+# Give jenkins user write access to the compilation folder to be able to create the required files for compilation
+sudo chown -R jenkins:jenkins /home/ubuntu/blockly-build/backend/compilation
+
 # go to backend and stop the previous app version and restart the new nodeJS app in back
 forever restart index.js -r dotenv/config || forever start index.js -r dotenv/config
 exit
