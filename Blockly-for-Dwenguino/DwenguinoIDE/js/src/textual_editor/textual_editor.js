2d61dae9a0ada9d0d8b8f5b31a85526a2e1c3713
Tom Neutens <tomneutens@gmail.com>
2022-01-28 12:37:44 +0100

    changed the default sound sensor pin to 15 + removed sound sensor block without pin selection

--- Blockly-for-Dwenguino/DwenguinoIDE/js/src/textual_editor/textual_editor.js
+++ Blockly-for-Dwenguino/DwenguinoIDE/js/src/textual_editor/textual_editor.js
@@ -14,14 +14,15 @@ class TextualEditor {
         let editorElement = $(`#${this.containerId}`); // Find the container element using jQuery
         editorElement.empty();  // Empty the element
         this.editor = monaco.editor.create(editorElement.get(0), {  // Add editor to element
-            value: code,
+            value: code.replace(/(\r\n|\r|\n){2}/g, '$1').replace(/(\r\n|\r|\n){3,}/g, '$1\n').trim(),
             language: 'cpp',
             roundedSelection: false,
             scrollBeyondLastLine: false,
             readOnly: false,
             theme: 'vs-dark'
-        })
-        
+        });
+        this.editor.getAction('editor.action.format').run()
+
     }
 
     getCurrentCode(){
