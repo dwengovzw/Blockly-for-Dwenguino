d6f9afd0f449cdc38e1c6b75f58d89e1779361c2
Tom Neutens <tomneutens@gmail.com>
2023-02-16 09:50:21 +0100

    Fixed invisible icons (issue #142)

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -68,7 +68,7 @@ module.exports = [
                 },
                 {
                     test: /\.ttf$/,
-                    use: ['file-loader']
+                    type: "asset/resource"
                 }
             ]
         },
--- webpack.config.dev.cjs
+++ webpack.config.dev.cjs
@@ -79,7 +79,7 @@ module.exports = [
                 },
                 {
                     test: /\.ttf$/,
-                    use: ['file-loader']
+                    type: "asset/resource"
                 }
             ]
         },
