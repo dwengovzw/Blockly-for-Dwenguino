52edd2f259517d94314f32ecc76088b56b5c9d54
Tom <tomneutens@gmail.com>
2023-05-04 16:25:01 +0200

    Integrating moveable components

--- Blockly-for-Dwenguino/dashboards/js/src/components/portfolios/graph_dashboard.ts
+++ Blockly-for-Dwenguino/dashboards/js/src/components/portfolios/graph_dashboard.ts
@@ -8,19 +8,37 @@ import { msg } from "@lit/localize";
 
 import { LitMoveable } from "lit-moveable";
 
-import "./items/moveable_item"
-
 @customElement("dwengo-graph-dashboard")
 class GraphDashboard extends connect(store)(LitElement){
     @property() uuid: string = ""
     @state() portfolio: PortfolioInfo | null = null
 
+    @query('.target')
+    target!: HTMLElement;
     @query(".container")
     container!: HTMLElement;
+    translation = [0, 0]
+
+    maxWidth: any = "400px";
+    maxHeight: any = "400px";
+    minWidth: any = "50px";
+    minHeight: any = "40px";
+    resizable: any = true;
+    keepRatio: any = false;
+    throttleResize: any = 1;
+    renderDirections: any = ["nw","n","ne","w","e","sw","s","se"];
+
+    constructor(){
+        super()
+        if (customElements.get("lit-moveable") == undefined){
+            customElements.define("lit-moveable", LitMoveable)
+        }
+    }
 
     stateChanged(state: any): void{
         console.log(state)
         this.portfolio = structuredClone(state.portfolio.selectedPortfolio)
+        
     }
 
     connectedCallback(): void {
@@ -28,17 +46,47 @@ class GraphDashboard extends connect(store)(LitElement){
         store.dispatch(getPortfolio(this.uuid))
     }
 
+    onDrag(e) {
+        e.detail.target.style.transform = e.detail.transform;
+        this.translate = e.detail.beforeTranslate;
+    }
+
+    onResize(e) {
+        const beforeTranslate = e.detail.drag.beforeTranslate
+        e.detail.target.style.width = `${e.detail.width}px`
+        e.detail.target.style.height = `${e.detail.height}px`
+        e.detail.target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`
+        this.translation = beforeTranslate
+    }
+
+    onDragStart(e) {
+        e.detail.set(this.translation)
+    }
+
+    onResizeStart(e) {
+        e.detail.dragStart && e.detail.dragStart.set(this.translation);
+    }
+
     render() {
         return html`
             <div class="root">
                 <div class="container">
-                <dwengo-moveable-item
-                    x="10"
-                    y="10"
-                    width="100"
-                    height="100">
-                    <span>Slot content</span>
-                </dwengo-moveable-item>     
+                    <div class="target">
+                            Target
+                    </div>
+                    <lit-moveable
+                      .target=${this.target}
+                      .litDraggable=${true}
+                      .throttleDrag=${1}
+                      .edgeDraggable=${false}
+                      .startDragRotate=${0}
+                      .throttleDragRotate=${0}
+                      .resizable=${this.resizable}
+                      .keepRatio=${this.keepRatio}
+                      @litDrag=${this.onDrag}
+                      .renderDirections=${this.renderDirections}
+                      @litResize=${this.onResize}
+                  ></lit-moveable>
             </div>
         `
     }
@@ -51,10 +99,68 @@ class GraphDashboard extends connect(store)(LitElement){
         overflow: hidden;
         border: 1px solid #ccc;
      }
- 
-    .root {
-        position: relative;
-    }
+     .margin .root {
+            position: static;
+        }
+        .description {
+            padding: 10px;
+        }
+        .root {
+            position: relative;
+        }
 
+        .target {
+            position: absolute;
+            width: 100px;
+            height: 100px;
+            top: 150px;
+            left: 100px;
+            line-height: 100px;
+            font-weight: bold;
+            border: 1px solid #333;
+            box-sizing: border-box;
+            overflow: hidden;
+        }
+        
+        
+        
+       
+        
+        
+        /* pos guidelines */
+        .moveable-normal.red {
+            background: red!important;
+        }
+        /* gap guidelines */
+        .moveable-gap.red {
+            background: red!important;
+        }
+        /* When snapped to an element in elementGuidelines */
+        .moveable-bold.red {
+            background: red!important;
+        }
+        /* A dashed line between target and element */
+        .moveable-dashed.red {
+            border-top-color: red!important;
+            border-left-color: red!important;
+        }
+        
+        /* pos guidelines */
+        .moveable-normal.green {
+            background: green!important;
+        }
+        /* gap guidelines */
+        .moveable-gap.green {
+            background: green!important;
+        }
+        /* When snapped to an element in elementGuidelines */
+        .moveable-bold.green {
+            background: green!important;
+        }
+        /* A dashed line between target and element */
+        .moveable-dashed.green {
+            border-top-color: green!important;
+            border-left-color: green!important;
+        }
      `
 }
--- Blockly-for-Dwenguino/dashboards/js/src/components/portfolios/items/moveable_item.ts
+++ Blockly-for-Dwenguino/dashboards/js/src/components/portfolios/items/moveable_item.ts
@@ -42,6 +42,8 @@ class MoveableItem extends LitElement {
 
     render() {
         return html`
+        <div class="root">
+                <div class="container">
                     <div class="target"
                          style=${`width:"${this.width}px" height:"${this.height}px" left:"${this.x}px" top:"${this.y}px"`}   >
                             <slot></slot>
@@ -59,9 +61,182 @@ class MoveableItem extends LitElement {
                       .renderDirections=${["nw","n","ne","w","e","sw","s","se"]}
                       @litResize=${this.onResize}
                   ></lit-moveable>
+                </div>
+                </div>
         `
     }
 
+    static styles: CSSResultGroup = css`
+    html, body {
+        position: relative;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    html:has(.no-relative), body:has(.no-relative) {
+        margin: 8px;
+        padding: 8px;
+        position: static;
+        /* border: 8px solid red; */
+    }
+    html:has(.no-relative) {
+        position: relative;
+    }
+    html:has(.margin), body:has(.margin) {
+        /* margin-top: 50px; */
+    }
+    .margin .root {
+        position: static;
+    }
+    .description {
+        padding: 10px;
+    }
+    .root {
+        position: relative;
+    }
+    .container {
+        position: relative;
+        margin-top: 50px;
+    }
+    .will-change-container {
+        padding-left: 100px;
+        padding-top: 100px;
+        will-change: transform;
+    }
+    .will-change-target {
+        position: relative;
+        width: 100px;
+        height: 100px;
+        line-height: 100px;
+        text-align: center;
+        background: #ee8;
+        color: #333;
+        font-weight: bold;
+        border: 1px solid #333;
+        box-sizing: border-box;
+    }
+    .target {
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        top: 150px;
+        left: 100px;
+        line-height: 100px;
+        text-align: center;
+        background: #ee8;
+        color: #333;
+        font-weight: bold;
+        border: 1px solid #333;
+        box-sizing: border-box;
+    }
+    
+    .target1 {
+        left: 120px;
+        top: 120px;
+    }
+    
+    .target2 {
+        left: 300px;
+        top: 140px;
+    }
+    
+    .target3 {
+        left: 180px;
+        top: 250px;
+    }
+    
+    .nested {
+        position: absolute;
+        border: 4px solid #ccc;
+        width: 100px;
+        height: 100px;
+        top: 50px;
+        left: 70px;
+        color: #333;
+        /* box-sizing: border-box; */
+    }
+    
+    .nested.first {
+        top: 150px;
+    }
+    
+    .nested.rotate {
+        transform-origin: 0 0;
+        transform: rotate(-30deg);
+    }
+    
+    .nested.scale {
+        transform: scale(1.5, 1.5);
+    }
+    
+    .nested .target {
+        top: 50px;
+        left: 50px
+    }
+    
+    
+    /* pos guidelines */
+    .moveable-normal.red {
+        background: red!important;
+    }
+    /* gap guidelines */
+    .moveable-gap.red {
+        background: red!important;
+    }
+    /* When snapped to an element in elementGuidelines */
+    .moveable-bold.red {
+        background: red!important;
+    }
+    /* A dashed line between target and element */
+    .moveable-dashed.red {
+        border-top-color: red!important;
+        border-left-color: red!important;
+    }
+    
+    /* pos guidelines */
+    .moveable-normal.green {
+        background: green!important;
+    }
+    /* gap guidelines */
+    .moveable-gap.green {
+        background: green!important;
+    }
+    /* When snapped to an element in elementGuidelines */
+    .moveable-bold.green {
+        background: green!important;
+    }
+    /* A dashed line between target and element */
+    .moveable-dashed.green {
+        border-top-color: green!important;
+        border-left-color: green!important;
+    }
+    
+    .scrollArea {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: calc(100vh - 100px);
+        overflow: auto;
+    }
+    .scrollArea:before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 300%;
+        width: 100%;
+        background: linear-gradient(
+            #333,
+            #fff
+        );
+    }
+    .infinite-viewer {
+        height: 500px;
+    }`
+
+    /*
     static styles: CSSResultGroup = css`
         :host {
             display: inline-block;
@@ -83,42 +258,46 @@ class MoveableItem extends LitElement {
             left: 10px;
             top: 10px;
         }
+
+        .container {
+            position: relative;
+            width: 100%;
+            height: 90vh;
+            overflow: hidden;
+            border: 1px solid #ccc;
+         }
+
+         .root {
+            position: relative;
+        }
         
-        /* pos guidelines */
         .moveable-normal.red {
             background: red!important;
         }
-        /* gap guidelines */
         .moveable-gap.red {
             background: red!important;
         }
-        /* When snapped to an element in elementGuidelines */
         .moveable-bold.red {
             background: red!important;
         }
-        /* A dashed line between target and element */
         .moveable-dashed.red {
             border-top-color: red!important;
             border-left-color: red!important;
         }
         
-        /* pos guidelines */
         .moveable-normal.green {
             background: green!important;
         }
-        /* gap guidelines */
         .moveable-gap.green {
             background: green!important;
         }
-        /* When snapped to an element in elementGuidelines */
         .moveable-bold.green {
             background: green!important;
         }
-        /* A dashed line between target and element */
         .moveable-dashed.green {
             border-top-color: green!important;
             border-left-color: green!important;
         }
         
-        `
+        `*/
 }
