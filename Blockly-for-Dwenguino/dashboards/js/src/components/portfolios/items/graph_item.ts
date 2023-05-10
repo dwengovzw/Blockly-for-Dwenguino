938fd0acc4a7a556b7cd7de37ced699578f92993
Tom <tomneutens@gmail.com>
2023-05-10 17:12:03 +0200

    Updated styles of portfolio page

--- Blockly-for-Dwenguino/dashboards/js/src/components/portfolios/items/graph_item.ts
+++ Blockly-for-Dwenguino/dashboards/js/src/components/portfolios/items/graph_item.ts
@@ -2,14 +2,15 @@
  * @author Tom Neutens <tomneutens@gmail.com>
  */
 
-import { LitElement, css, html, CSSResultGroup } from "lit";
-import {customElement, property, state} from 'lit/decorators.js';
+import { LitElement, css, html, CSSResultGroup, PropertyValueMap } from "lit";
+import {customElement, property, state, query} from 'lit/decorators.js';
 import { store } from "../../../state/store"
 import { msg } from '@lit/localize';
 import { connect } from "pwa-helpers"
 import { getGoogleMateriaIconsLinkTag } from "../../../util"
 import { noselect } from "../../../styles/shared";
 import { deletePortfolioItem, PortfolioItemInfo } from "../../../state/features/portfolio_slice";
+import { buttonStyles, iconStyle } from "../../../styles/shared";
 
 @customElement("dwengo-graph-portfolio-item")
 class PortfolioItem extends connect(store)(LitElement) {
@@ -18,7 +19,8 @@ class PortfolioItem extends connect(store)(LitElement) {
     @property({type: String})
     portfolioUUID = ""
 
-    @state() showDialog = false
+    @query(".item_detail_modal")
+    itemDetailModal!: HTMLDialogElement
 
     private itemToIconMap = {
         "TextItem": "text_fields",
@@ -26,6 +28,19 @@ class PortfolioItem extends connect(store)(LitElement) {
         "SocialRobotDesignItem": "draw",
         "default": "unknown_med"
     }
+
+    protected firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+        super.firstUpdated(_changedProperties);
+        this.itemDetailModal.addEventListener("click", (e) => {
+            if(e.target === this.itemDetailModal){
+                this.itemDetailModal.close()
+            }
+        })
+        window.addEventListener("mouseup", (e) => {
+            this.onMouseUp(e)
+        })
+    }
+
 
     mapItemToElement(){
         const itemType: string = this.item?.__t || ""
@@ -43,9 +58,7 @@ class PortfolioItem extends connect(store)(LitElement) {
 
     protected render() {
         return html`
-            <div 
-                @mouseup=${this.onMouseUp}
-                class="container noselect">
+            <div class="container noselect">
                 ${getGoogleMateriaIconsLinkTag()}
                 <div class="item_header">
                     <span class="material-symbols-outlined portfolio_item_handle"
@@ -53,21 +66,23 @@ class PortfolioItem extends connect(store)(LitElement) {
                     >
                         drag_indicator
                     </span>
-                    <span class="material-symbols-outlined portfolio_add_feedback">
+                    <span class="material-symbols-outlined dwengo-icon">
                         ${this.itemToIconMap[this.item?.__t || "default"]}
                     </span>
                     <span 
-                        class="material-symbols-outlined portfolio_add_feedback"
+                        class="material-symbols-outlined dwengo-icon"
                         @click=${this.handleItemDetailClick}>
                             info
                     </span>
-                    <span class="material-symbols-outlined portfolio_add_feedback">
+                    <span class="material-symbols-outlined dwengo-icon">
                         rate_review
                     </span>
-                    
+                    <span class="material-symbols-outlined  dwengo-icon">
+                        adjust
+                    </span>
                 </div>
             </div>
-            ${this.showDialog ? this.renderItemDetailModal() : ""}
+            ${this.renderItemDetailModal()}
             
         `
     }
@@ -78,11 +93,12 @@ class PortfolioItem extends connect(store)(LitElement) {
         const itemType: string = this.item?.__t || ""
         switch(itemType){
             case "TextItem":
-                this.showDialog = true
             case "BlocklyProgSequenceItem":
-                return html`<dwengo-portfolio-blockly-code-item portfolioUUID=${this.portfolioUUID} item=${JSON.stringify(this.item)}></dwengo-portfolio-blockly-code-item>`
+                this.itemDetailModal.showModal()
+                break
             case "SocialRobotDesignItem":
                 console.log("Not supported yet")
+                break
             default:    
                 return html`${msg("Unknown item type")}`
         }
@@ -90,8 +106,23 @@ class PortfolioItem extends connect(store)(LitElement) {
 
     renderItemDetailModal(){
         return html`
-            <dialog open=true class="item_detail_modal">
-                ${this.mapItemToElement()}
+            <dialog class="item_detail_modal">
+                <div class="dialog_content_container">
+                    <div class="dialog_header">
+                        <form method="dialog">
+                            <button type="submit" autofocus class="dwengo-button">
+                                <span class="dwengo-button-icon material-symbols-outlined">
+                                    close
+                                </span>
+                            </button>
+                        </form>
+                    </div>
+                    <div class="dialog_content">
+                        ${this.mapItemToElement()}
+                    </div>
+                </div>
+                
+                
             </dialog>`
     }
 
@@ -122,15 +153,25 @@ class PortfolioItem extends connect(store)(LitElement) {
     }
 
     static styles: CSSResultGroup = [css`
+
+        .item_detail_modal {
+            padding: 0;
+        }
         .portfolio_item_handle {
             cursor: move;
         }
-        .portfolio_add_feedback {
-            background-color: var(--theme-accentFillActive);
-            border-radius: 2.5px;
-            padding: 2.5px;
+        .dialog_content_container {
+            min-width: 60vw;
+            margin: 1rem;
         }
-        `, noselect]
+        .dialog_header {
+            display: flex;
+            position: absolute;
+            justify-content: flex-end;
+            width: calc(100% - 2rem);
+            height: 35px;
+        }
+        `, noselect, buttonStyles, iconStyle]
 
 }
 
--- Blockly-for-Dwenguino/dashboards/js/src/styles/shared.ts
+++ Blockly-for-Dwenguino/dashboards/js/src/styles/shared.ts
@@ -9,3 +9,30 @@ export const noselect = css`
               user-select: none; /* Non-prefixed version, currently
                                     supported by Chrome, Edge, Opera and Firefox */
   }`
+
+  export const buttonStyles = css`
+    .dwengo-button {
+        background-color: var(--theme-accentFillSelected);
+        border-radius: 2.5px;
+        padding: 2.5px;
+        margin: 2.5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: none;
+    }
+    .dwengo-button-icon {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: var(--theme-white);
+    }`
+
+    export const iconStyle = css`
+        .dwengo-icon {
+            background-color: var(--theme-accentFillSelected);
+            border-radius: 2.5px;
+            padding: 2.5px;  
+            color: white;
+            font-weight: 300;
+        }`
