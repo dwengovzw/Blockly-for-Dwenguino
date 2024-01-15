17db0a386552e492ae7094fff1ac68b65a033d3a
Tom <tomneutens@gmail.com>
2024-01-15 16:02:20 +0100

    Added logging for production deploy

--- deploy/docker_deploy.sh
+++ deploy/docker_deploy.sh
@@ -15,10 +15,10 @@ cd /home/node/deploy
 #ls /home/node/deploy
 
 # Give jenkins user write access to the compilation folder to be able to create the required files for compilation
-sudo chown -R node:node /home/node/deploy/backend/compilation
+sudo chown -R node:node /home/node/deploy/backend/compilation > blockly.log 2> blockly_err.log
 
 # go to backend and stop the previous app version and restart the new nodeJS app in back
-npm run start
+npm run start 
 #forever restart backend/index.js -r dotenv/config || forever start backend/index.js -r dotenv/config
 exit
 
