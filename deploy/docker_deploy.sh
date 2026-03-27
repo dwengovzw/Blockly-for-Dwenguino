578ff29c9394f7f027972359044bc04f96d179b2
Tom Neutens <tomneutens@gmail.com>
2026-03-27 12:04:02 +0100

    updated deploy script

--- deploy/docker_deploy.sh
+++ deploy/docker_deploy.sh
@@ -6,7 +6,7 @@ echo $env
 
 echo "changing to deploy directory"
 # Go to deploy directory
-#cd /home/node/deploy
+cd /home/node/deploy
 
 
 
@@ -19,11 +19,11 @@ echo "changing to deploy directory"
 
 echo "change rights"
 # Give jenkins user write access to the compilation folder to be able to create the required files for compilation
-#chown -R node:node /home/node/deploy/backend/compilation 
+chown -R node:node /home/node/deploy/backend/compilation 
 
 echo "run application"
 # go to backend and stop the previous app version and restart the new nodeJS app in back
-#npm run start:dev > blockly.log 2> blockly_err.log
+npm run start:dev > blockly.log 2> blockly_err.log
 #forever restart backend/index.js -r dotenv/config || forever start backend/index.js -r dotenv/config
 exit
 
