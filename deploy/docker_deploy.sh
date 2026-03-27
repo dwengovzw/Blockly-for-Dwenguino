34e1dee76c3c547d5c50ca4f5858009fa77ae564
Tom Neutens <tomneutens@gmail.com>
2026-03-27 12:10:05 +0100

    updated deploy script

--- deploy/docker_deploy.sh
+++ deploy/docker_deploy.sh
@@ -22,8 +22,9 @@ echo "change rights"
 chown -R node:node /home/node/deploy/backend/compilation 
 
 echo "run application"
+exec npm run start:dev
 # go to backend and stop the previous app version and restart the new nodeJS app in back
-npm run start:dev > blockly.log 2> blockly_err.log
+#npm run start:dev > blockly.log 2> blockly_err.log
 #forever restart backend/index.js -r dotenv/config || forever start backend/index.js -r dotenv/config
-exit
+#exit
 
