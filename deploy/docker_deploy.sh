2033d602c4d9225b98a65a77b1b93ca845d70746
Tom Neutens <tomneutens@gmail.com>
2026-03-27 15:48:49 +0100

    Added install of adafruit toolchain in deploy script

--- deploy/docker_deploy.sh
+++ deploy/docker_deploy.sh
@@ -12,6 +12,12 @@ echo "change rights"
 # Give jenkins user write access to the compilation folder to be able to create the required files for compilation
 chown -R node:node /home/node/deploy/backend/compilation 
 
+echo "installing arm-none-eabi-gcc toolchain for Halberd compilation"
+ARDUINO_CLI="/home/node/deploy/backend/compilation/bin/linux/arduino-cli"
+"$ARDUINO_CLI" config init --overwrite --additional-urls https://adafruit.github.io/arduino-board-index/package_adafruit_index.json
+"$ARDUINO_CLI" core update-index
+"$ARDUINO_CLI" core install adafruit:nrf52
+
 echo "run application"
 exec npm run start:dev
 # go to backend and stop the previous app version and restart the new nodeJS app in back
