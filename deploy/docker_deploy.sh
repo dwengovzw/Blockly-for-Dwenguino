bf7a96ddc7373555dc0b9905a125b857b2b98267
Tom Neutens <tomneutens@gmail.com>
2026-03-27 11:11:08 +0100

    Added more debug info

--- deploy/docker_deploy.sh
+++ deploy/docker_deploy.sh
@@ -4,8 +4,11 @@
 env=${1:-dev.env}
 echo $env
 
+echo "changing to deploy directory"
 # Go to deploy directory
 cd /home/node/deploy
+
+
 
 # Copy the .env files from the config directory on the server into the deploy directory
 #cp /var/environments/$env /home/node/deploy/.env
@@ -14,9 +17,11 @@ cd /home/node/deploy
 
 #ls /home/node/deploy
 
+echo "change rights"
 # Give jenkins user write access to the compilation folder to be able to create the required files for compilation
 chown -R node:node /home/node/deploy/backend/compilation 
 
+echo "run application"
 # go to backend and stop the previous app version and restart the new nodeJS app in back
 npm run start > blockly.log 2> blockly_err.log
 #forever restart backend/index.js -r dotenv/config || forever start backend/index.js -r dotenv/config
